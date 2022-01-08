b609c5ba5fddef6ddac7c2e20bd35e0f7c65245b
Jasmine Durham <jasminedurham@Jasmines-MacBook-Air.local>
2022-01-08 16:04:25 -0500

    first check in

---
+++ src/main/java/durham/jasmine/imageshare/ImageshareApplication.java
@@ -0,0 +1,13 @@
+package durham.jasmine.imageshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageshareApplication.class, args);
+	}
+
+}
